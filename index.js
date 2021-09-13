797569b00a1b135d59bd5f8a408eb41ffe9c83af
manju2052 <manju.2052.2052@gmail.com>
2021-09-14 00:11:54 +0530

    port updt

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const testRoute = require("./routes/testRoute");
 app.use(cors());
 app.use(testRoute);
 
-app.listen(4000, () => {
-  console.log("Server Run on PORT 4000");
+app.listen(8000, () => {
+  console.log("Server Run on PORT 8000");
 });
