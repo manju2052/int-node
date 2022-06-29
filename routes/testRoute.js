36b2fffe5923d1f8d893cb774738e10d8335ce8b
manju2052 <86000111+manju2052@users.noreply.github.com>
2022-06-29 19:51:18 +0530

    Update testRoute.js

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.send({ data: "HELLO WORLD PART MASTER" });
 });
+router.get("/test2", (req, res) => {
+  res.send({ data: "I am sayan" });
+});
 
 module.exports = router;
